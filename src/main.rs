5fde2442c7c830e31811431de31b29a92cb41a83
collinmurd <collinmurdock22@gmail.com>
2024-11-08 17:05:11 -0500

    basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about = "Webhook forwarding for GitHub Enterprise Server", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Forward webhooks to a local process
+    Forward {
+        /// Optional organization name
+        #[arg(short, long)]
+        arg: Option<String>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Forward { arg } => {
+            println!("Forwarding to {}...", arg.unwrap_or("not provided".to_string()));
+        }
+    }
+}
+
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert();
 }
