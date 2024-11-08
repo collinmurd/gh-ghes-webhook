bc1b119e40798224ab2c88b0e512ec0492e2018c
collinmurd <collinmurdock22@gmail.com>
2024-11-08 17:46:58 -0500

    replicate gh webhook args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{Parser, Subcommand};
+use clap::{Args, Parser, Subcommand};
 
 #[derive(Parser)]
 #[command(version, about = "Webhook forwarding for GitHub Enterprise Server", long_about = None)]
@@ -11,18 +11,45 @@ struct Cli {
 enum Commands {
     /// Forward webhooks to a local process
     Forward {
-        /// Optional organization name
-        #[arg(short, long)]
-        arg: Option<String>,
+        /// Names of the event types to forward. Use `*` to forward all events.
+        #[arg(short='E', long, num_args=1.., value_delimiter=' ')]
+        events: Vec<String>,
+
+        /// GitHub host name (default "github.com")
+        #[arg(short='H', long, default_value_t=String::from("github.com"))]
+        github_host: String,
+
+        #[command(flatten)]
+        location: WebhookLocation,
+
+        /// Webhook secret for incoming events
+        #[arg(short='S', long)]
+        secret: Option<String>,
+
+        /// Address of the local server to receive events. If omitted, events will be printed to stdout
+        #[arg(short='U', long)]
+        url: Option<String>,
     },
+}
+
+#[derive(Args)]
+#[group(required = true, multiple=false)]
+struct WebhookLocation {
+        /// Name of the org where the webhook is installed
+        #[arg(short='O', long)]
+        org: String,
+
+        /// Name of the repo where the webhook is installed
+        #[arg(short='R', long)]
+        repo: String,
 }
 
 fn main() {
     let cli = Cli::parse();
 
     match cli.command {
-        Commands::Forward { arg } => {
-            println!("Forwarding to {}...", arg.unwrap_or("not provided".to_string()));
+        Commands::Forward {events, github_host, location, secret, url} => {
+            println!("Forwarding...");
         }
     }
 }
